e20780bf147ee13ad21269dbf31bb7582823bae0
Arihant Jain <thearijain@gmail.com>
2022-02-02 15:03:50 -0500

    Initial Commit

---
+++ MongoDBTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MongoDBTest
+//
+//  Created by Ari Jain on 2/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
